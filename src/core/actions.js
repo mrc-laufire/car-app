fb6094bce1d9b7c4888bf98caa25b7dc03bd15b1
mrc-laufire <mrc@laufire.in>
2021-09-24 10:28:22 +0000

    Added add car and reset state actions.

--- src/core/actions.js
+++ src/core/actions.js
@@ -1,3 +1,5 @@
+import carManager from '../services/carManager';
+
 const setMake = ({ data }) => ({
 	make: data,
 });
@@ -14,11 +16,22 @@ const setPurchaseDate = ({ data }) => ({
 	purchaseDate: data,
 });
 
+const addCar = ({ state }) => ({
+	cars: carManager.addCar(state),
+});
+
+const resetState = ({ state, data }) => ({
+	...data,
+	cars: state.cars,
+});
+
 const actions = {
 	setMake,
 	setModel,
 	setVehicleNumber,
 	setPurchaseDate,
+	addCar,
+	resetState,
 };
 
 export default actions;
--- src/core/actions.test.js
+++ src/core/actions.test.js
@@ -1,4 +1,5 @@
 /* eslint-disable max-lines-per-function */
+import carManager from '../services/carManager';
 import actions from './actions';
 
 describe('Actions', () => {
@@ -25,5 +26,23 @@ describe('Actions', () => {
 		const result = actions.setPurchaseDate({ data: purchaseDate });
 
 		expect(result).toEqual({ purchaseDate });
+	});
+	test('addCar', () => {
+		const addCar = Symbol('addCar');
+
+		jest.spyOn(carManager, 'addCar')
+			.mockReturnValue(addCar);
+
+		const result = actions.addCar({ state: {}});
+
+		expect(result).toEqual({ cars: addCar });
+	});
+	test('resetState', () => {
+		const resetState = { seed: Symbol('resetState') };
+		const cars = Symbol('cars');
+		const result = actions
+			.resetState({ state: { cars }, data: resetState });
+
+		expect(result).toEqual({ ...resetState, cars });
 	});
 });
