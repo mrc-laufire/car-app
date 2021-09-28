511124c9c52ef4ca07a74c053beb427dfb9d0ae4
mrc-laufire <mrc@laufire.in>
2021-09-28 04:59:24 +0000

    Added removeCar function in service.

--- src/services/carManager.js
+++ src/services/carManager.js
@@ -4,9 +4,13 @@ const addCar = ({ make, model, vehicleNumber, purchaseDate, cars }) =>
 const isEmpty = ({ make, model, vehicleNumber, purchaseDate }) =>
 	[make, model, vehicleNumber, purchaseDate].includes('');
 
+const removeCar = ({ cars }, vehicleNumber) =>
+	cars.filter((car) => car.vehicleNumber !== vehicleNumber);
+
 const carManager = {
 	addCar,
 	isEmpty,
+	removeCar,
 };
 
 export default carManager;
--- src/services/carManager.test.js
+++ src/services/carManager.test.js
@@ -1,4 +1,7 @@
+/* eslint-disable max-nested-callbacks */
+/* eslint-disable max-lines-per-function */
 import carManager from './carManager';
+import { rndBetween } from '@laufire/utils/random';
 
 describe('carManager', () => {
 	const model = Symbol('model');
@@ -18,5 +21,33 @@ describe('carManager', () => {
 		const result = carManager.isEmpty(state);
 
 		expect(result).toEqual(false);
+	});
+	describe('removeCar', () => {
+		const hundred = 100;
+		const thousand = 1000;
+		const rndNumber = rndBetween(1, hundred);
+
+		test('returns the mockValue', () => {
+			const mockReturn = Symbol('removeCar');
+
+			jest.spyOn(cars, 'filter').mockReturnValue(mockReturn);
+
+			const result = carManager.removeCar({ cars }, rndNumber);
+
+			expect(result).toEqual(mockReturn);
+		});
+
+		test('returns the cars without mentioned vehicleNumber', () => {
+			const mockCars = [
+				{ ...state, vehicleNumber: rndNumber },
+				{ ...state, vehicleNumber: thousand },
+			];
+			const expectedResult = mockCars
+				.filter((car) => car.vehicleNumber !== rndNumber);
+
+			const result = carManager.removeCar({ cars: mockCars }, rndNumber);
+
+			expect(result).toEqual(expectedResult);
+		});
 	});
 });
