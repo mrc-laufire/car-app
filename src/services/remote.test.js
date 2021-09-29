bc857ac32d901163ec7685837e21060a7cd410b1
mrc-laufire <mrc@laufire.in>
2021-09-29 04:09:16 +0000

    Refactored remote.fetch test.

--- src/services/remote.test.js
+++ src/services/remote.test.js
@@ -3,15 +3,18 @@ import context from '../core/context';
 import Remote from './remote';
 
 describe('Remote', () => {
-	test('fetch', () => {
+	test('fetch', async () => {
 		const data = Symbol('data');
 		const mockValue = { data };
 
 		jest.spyOn(axios, 'get').mockReturnValue(mockValue);
+
 		jest.spyOn(context.actions, 'updateCars');
 
-		const result = Remote.fetch();
+		const result = await Remote.fetch();
 
-		expect(result);
+		expect(result).toBeUndefined();
+		expect(axios.get).toHaveBeenCalledWith('http://localhost:4000/cars');
+		expect(context.actions.updateCars).toHaveBeenCalledWith(data);
 	});
 });
