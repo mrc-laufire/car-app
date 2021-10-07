4cef1f884fbd146251cfc7cfa26bf228af7879b2
mrc-laufire <mrc@laufire.in>
2021-10-07 11:26:21 +0000

    Unified actions test.

--- src/core/actions.test.js
+++ src/core/actions.test.js
@@ -1,8 +1,5 @@
-/* eslint-disable max-statements */
 /* eslint-disable max-lines-per-function */
 import carManager from '../services/carManager';
-import { range } from '@laufire/utils/collection';
-import { rndValue } from '@laufire/utils/random';
 import TestHelpers from '../../test/helpers';
 import actions from './actions';
 
@@ -10,13 +7,19 @@ describe('Actions', () => {
 	const { rndString } = TestHelpers;
 
 	const expectations = [
-		['setVehicleNumber', 'vehicleNumber', rndString()],
-		['setPurchaseDate', 'purchaseDate', rndString()],
+		['setVehicleNumber', 'vehicleNumber'],
+		['setPurchaseDate', 'purchaseDate'],
+		['setCars', 'cars'],
+		['setBrands', 'brands'],
+		['setMakes', 'makes'],
+		['setMake', 'make'],
+		['setModels', 'models'],
+		['setModel', 'model'],
 	];
 
-	test.each(expectations)('%p', (
-		action, key, data
-	) => {
+	test.each(expectations)('%p', (action, key) => {
+		const data = rndString();
+
 		const result = actions[action]({ data });
 
 		expect(result[key]).toEqual(data);
@@ -54,56 +57,6 @@ describe('Actions', () => {
 
 		const result = actions
 			.removeCar({ state: state, data: vehicleNumber });
-
-		expect(result).toEqual(expectedResult);
-	});
-	test('set', () => {
-		const min = 0;
-		const max = 3;
-		const data = range(min, max).map(() =>
-			({ make: rndString(), model: rndString() }));
-
-		const result = actions.setCars({ data });
-
-		expect(result).toEqual({ cars: data });
-	});
-	test('setBrands', () => {
-		const data = { Audi: ['Q8', 'R8'] };
-		const expectedResult = { brands: data };
-
-		const result = actions.setBrands({ data });
-
-		expect(result).toEqual(expectedResult);
-	});
-	test('setMakes', () => {
-		const data = ['Audi', 'BMW', 'Benz'];
-		const expectedResult = { makes: data };
-
-		const result = actions.setMakes({ data });
-
-		expect(result).toEqual(expectedResult);
-	});
-	test('setMake', () => {
-		const data = rndValue(['Audi', 'BMW', 'Benz']);
-		const expectedResult = { make: data };
-
-		const result = actions.setMake({ data });
-
-		expect(result).toEqual(expectedResult);
-	});
-	test('setModels', () => {
-		const data = ['M3', '320d', '520d'];
-		const expectedResult = { models: data };
-
-		const result = actions.setModels({ data });
-
-		expect(result).toEqual(expectedResult);
-	});
-	test('setModel', () => {
-		const data = rndValue(['M3', '320d', '520d']);
-		const expectedResult = { model: data };
-
-		const result = actions.setModel({ data });
 
 		expect(result).toEqual(expectedResult);
 	});
